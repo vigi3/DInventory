ada54aa7ce8b94ed7e34b24534980b2a89837467
vigi3 <vincentpro52@gmail.com>
2021-03-31 14:17:28 +0200

    new method, check if two username are equal in User.php

--- Model/User.php
+++ Model/User.php
@@ -5,23 +5,38 @@ require_once 'Model/Model.php';
 class User extends Model {
 
     /** 
-     * @return string $usernameRetrieved
-     * The username to sign-in
+     * @return string $nameRetrieved
+     * The name of a user
     */
-    public function getUsername(string $username) {
-        $sql = 'SELECT Username FROM user WHERE Username=?';
-        $usernameRetrieved = $this->RequestDB($sql);
-        return $usernameRetrieved;
+    public function getName(string $name) {
+        $sql = 'SELECT Name FROM user WHERE Name=?';
+        $nameRetrieved = $this->RequestDB($sql, $name);
+        return $nameRetrieved;
     }
 
     /** 
      * @return User object $userCreationReq
      * The new User created
     */
-    public function newUser(string $name, string $lastName, string $username, string $password, int $role) {
+    public function newUser(string $name, string $lastName, int $role, string $username, string $password) {
         $sql = 'INSERT INTO user (Name, Lastname, Role, Username, Password) VALUES (?, ?, ?, ?, ?)';
-        $userCreationReq = $this->RequestDB($sql, $name, $lastName, $username, $password);
+        $userCreationReq = $this->RequestDB($sql, $name, $lastName, $role, $username, $password);
         return $userCreationReq;
+    }
+
+    /** 
+     * @return bool
+     * True if $username = $usernameRetrieved
+    */
+    public function usernameEquals(string $username): bool {
+        $sql = 'SELECT Username FROM user WHERE Username=?';
+        $usernameRetrieved = $this->RequestDB($sql, $username);
+        if($usernameRetrieved === $username){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     /** 
@@ -32,7 +47,7 @@ class User extends Model {
     public function passwordEquals(string $password): bool {
         $sql = 'SELECT Password FROM user WHERE Password=?';
         $passwordRetrieved = $this->RequestDB($sql, $password);
-        if(!$passwordRetrieved == null){
+        if($passwordRetrieved === $password){
             return true;
         }
         else {
