89f39c5dc421e133783ba216a92ab872a3554451
vigi3 <vincentpro52@gmail.com>
2021-04-14 11:11:44 +0200

    Variable title is sent to MainPage

--- Controller/MainPageController.php
+++ Controller/MainPageController.php
@@ -6,6 +6,8 @@ require_once 'Vue/Vue.php';
 class MainPageController {
 
     private ?string $nameUser = null;
+    private string $titlePage = 'Accueil';
+    private  $titleArray = array('title'=>'Accueil');
 
 
     public function mainPageVue(string $title) 
@@ -14,7 +16,7 @@ class MainPageController {
         $_SESSION['title'] = 'Accueil';
         $_SESSION['name'] = $this->getName($_POST['username']);
         $vue = new Vue('MainPage');
-        $vue->generate(array('title'=>$title, 'nameUser'=>$this->nameUser));
+        $vue->generate(array('title'=>$this->titleArray, 'nameUser'=>$this->nameUser));
 
     }
 
@@ -24,6 +26,12 @@ class MainPageController {
         $UserInSession = new User();
         $nameUser = $UserInSession->getName($usernameArray);
         return $nameUser[0];
+    }
+
+    public function getTitlePage(): string
+    {
+        $titleMainPage = $this->titlePage;
+        return $titleMainPage;
     }
 
 
--- Vue/vueMainPage.php
+++ Vue/vueMainPage.php
@@ -1,4 +1,6 @@
-<?php $this->title='Accueil';
-$this->nameUser = $_SESSION['name'];?>
+
+<?php $this->nameUser = $_SESSION['name'];?>
+
+<h2 id="titleTest"><?php $this->title = $title['title'] ?></h2>
 
   
