5c2ccb117acbd3757912999f6a4b6a9a51095a79
vigi3 <vincentpro52@gmail.com>
2021-04-14 18:16:10 +0200

    Added UI stat Items

--- Vue/template.php
+++ Vue/template.php
@@ -15,32 +15,98 @@
         <link rel="stylesheet" href="CSS/style.css">
         <title><?= $title ?></title>
     </head>
-    <body class="vh-100">
-        <nav class="nav d-inline-flex flex-column nav-justified bg-dark vh-100 pt-5" id="sticky-sidebar"> <!-- NavBar -->
+    <body class="vh-100 mainBackground">
+        <!-- NavBar -->
+        <nav class="nav d-inline-flex flex-column nav-justified bg-dark vh-100 pt-5" id="sticky-sidebar"> 
             <a class="nav-link link-light" aria-current="page" href="#"><h2><i class="bi bi-app"></i></i></h1></a> <!-- Home -->
             <a class="nav-link link-light" href="#"><h2><i class="bi bi-box"></i></h2></a> <!-- Products -->
             <a class="nav-link link-light" href="#"><h2><i class="bi bi-calendar3"></i></h2></a> <!-- Reservation -->
             <a class="nav-link link-light" href="#" tabindex="-1" aria-disabled="true"><h2><i class="bi bi-plus-circle"></i></h2></a> <!-- Add product -->
             <a class="nav-link link-light" href="#" tabindex="-1" aria-disabled="true"><h2><i class="bi bi-person-x"></i></h2></a> <!-- Log out -->
             <a class="nav-link link-light" href="#" tabindex="-1" aria-disabled="true"><h2><i class="bi bi-gear"></i></h2></a> <!-- Setting/Add User -->
-        </nav>  <!-- EndNavBar -->
-        <div class="container-fluid"> <!-- Container -->
-            <div class="row">
-                <div class="col-1" style="background-color: red;">
+        </nav>  
+        <!-- EndNavBar -->
+        <!-- Container -->
+        <div class="container-fluid">
+            <!-- Title -->
+            <div class="row py-5">
+                <div class="col-1">
                 </div>
-                <div class="col-11 align-self-start" style="background-color: blue;"> <!--  -->
-                    <h1 class="title text-capitalize">Bienvenue, <?= $nameUser ?></h1>
+                <div class="col-11 align-self-start"> 
+                    <h1 class="title text-capitalize fontSizeWelcome">Bienvenue, <?= $nameUser ?></h1>
                     <h5 class="fw-light fst-italic text-secondary">Gestionnaire de materiel</h5>
                     <p><?= $content ?></p>
-                </div> <!--  -->
+                </div> 
             </div>
-                
-            
-        </div> <!-- EndContainer -->
+            <!-- EndTitle -->
+
+            <!-- Graph -->
+            <div class="row">
+                <div class="col-1"></div>
+                <!-- Graph Black -->
+                <div class="col-2 px-4">
+                    <div class="card graphBlack text-white text-center graphCorner shadow">
+                        <div class="card-body">
+                            <p class="card-subtitle text-muted">Outils Réservés: </p>
+                            <h3 class="card-title"><a href="#" class="link link-light text-decoration-none">###</a></h3>
+                            <p class="card-subtitle text-muted">Items</p>
+                        </div>
+                    </div>  
+                </div>
+                <!-- EndGraph Black -->
+
+                <!-- Graph Yellow -->
+                <div class="col-2 px-4">
+                    <div class="card graphYellow text-white text-center graphCorner shadow">
+                        <div class="card-body">
+                            <p class="card-subtitle text-muted">Stock Total Restant: </p>
+                            <h3 class="card-title"><a href="#" class="link link-light text-decoration-none">###</a></h3>
+                            <p class="card-subtitle text-muted">Items</p>
+                        </div>
+                    </div>  
+                </div>
+                <!-- EndGraph Yellow -->
+
+                <!-- Graph Orange -->
+                <div class="col-2 px-4">
+                    <div class="card graphOrange text-white text-center graphCorner shadow">
+                        <div class="card-body">
+                            <p class="card-subtitle text-muted">Stock Faible: </p>
+                            <h3 class="card-title"><a href="#" class="link link-light text-decoration-none">###</a></h3>
+                            <p class="card-subtitle text-muted">Items</p>
+                        </div>
+                    </div>  
+                </div>
+                <!-- EndGraph Orange -->
+
+                <!-- Graph Red -->
+                <div class="col-2 px-4">
+                    <div class="card graphRed text-white text-center graphCorner shadow">
+                        <div class="card-body">
+                            <p class="card-subtitle text-muted">Rupture de Stock: </p>
+                            <h3 class="card-title"><a href="#" class="link link-light text-decoration-none">###</a></h3>
+                            <p class="card-subtitle text-muted">Items</p>
+                        </div>
+                    </div>  
+                </div>
+                <!-- EndGraph Red -->
+            </div>
+            <!-- EndGraph -->
+
+            <!-- Footer -->
+            <div class="row pt-5">
+                <div class="col-1"></div>
+                <div class="col-3">
+                    <footer id="piedBlog">
+                        Blog réalisé avec PHP, HTML5 et CSS.
+                    </footer>
+                </div>
+            </div>
+            <!-- EndFooter -->
+        </div> 
+        <!-- EndContainer -->
         
-        <footer id="piedBlog">
-            Blog réalisé avec PHP, HTML5 et CSS.
-        </footer>
+        
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     </body>
     
